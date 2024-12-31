984d3c3833392ee4f8159c008c08f14eed75d4d9
Zeming Hui <me@huizm.cn>
2024-12-31 15:51:25 +0800

    Implement Day 23 Task 3

--- src/handlers/day_23.rs
+++ src/handlers/day_23.rs
@@ -1,8 +1,23 @@
-use axum::{http::header, response::IntoResponse};
+use axum::{extract::Path, http::StatusCode, response::Html};
 
-pub async fn star() -> impl IntoResponse {
-    (
-        [(header::CONTENT_TYPE, "text/html")],
-        r#"<html><div id="star" class="lit"></div></html>"#,
-    )
+pub async fn star() -> Html<&'static str> {
+    Html(r#"<html><div id="star" class="lit"></div></html>"#)
+}
+
+pub async fn color(Path(color): Path<String>) -> Result<Html<String>, StatusCode> {
+    Ok(Html(format!(r#"
+        <html>
+            <div class="present {}" hx-get="/23/present/{}" hx-swap="outerHTML">
+                <div class="ribbon"></div>
+                <div class="ribbon"></div>
+                <div class="ribbon"></div>
+                <div class="ribbon"></div>
+            </div>
+        </html>
+    "#, color, match color.as_str() {
+        "red" => "blue",
+        "blue" => "purple",
+        "purple" => "red",
+        _ => { return Err(StatusCode::IM_A_TEAPOT); },
+    })))
 }
--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,8 @@ async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::Shut
         .route("/19/undo/:id", put(handlers::undo)).with_state(pool.clone())
         .route("/19/draft", post(handlers::draft)).with_state(pool.clone())
         .nest_service("/assets", ServeDir::new("assets"))
-        .route("/23/star", get(handlers::star));
+        .route("/23/star", get(handlers::star))
+        .route("/23/present/:color", get(handlers::color));
 
     Ok(router.into())
 }
