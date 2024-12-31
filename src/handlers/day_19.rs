9c5c58757b1e41d008b5655bcf9d0ea2bf9eb708
Zeming Hui <me@huizm.cn>
2024-12-31 13:26:39 +0800

    Complete Day 19 core tasks

--- src/handlers/day_19.rs
+++ src/handlers/day_19.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use axum::{extract::{State, Path, Json}, http::StatusCode};
 use serde::{Deserialize, Serialize};
-use sqlx::{types::{chrono::Local,  Uuid}, PgPool};
+use sqlx::{types::Uuid, PgPool};
 
 use crate::models::Quote;
 
@@ -26,7 +26,7 @@ pub async fn cite(
     Path(id): Path<Uuid>,
 ) -> Result<Json<Quote>, StatusCode>
 {
-    match sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = ?")
+    match sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = $1")
         .bind(id)
         .fetch_one(&*pool)
         .await
@@ -46,13 +46,13 @@ pub async fn remove(
     let mut tx = pool.begin().await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    let q = sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = ?")
+    let q = sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = $1")
         .bind(id)
         .fetch_one(&mut *tx)
         .await
         .map_err(|_| StatusCode::NOT_FOUND)?;
 
-    match sqlx::query("DELETE FROM quotes WHERE id = ?")
+    match sqlx::query("DELETE FROM quotes WHERE id = $1")
         .bind(id)
         .execute(&mut *tx)
         .await
@@ -75,7 +75,7 @@ pub async fn undo(
     Json(req): Json<QuoteReq>,
 ) -> Result<Json<Quote>, StatusCode>
 {
-    match sqlx::query_as::<_, Quote>("UPDATE quotes SET author = ?, quote = ?, version = version + 1 WHERE id = ? RETURNING *")
+    match sqlx::query_as::<_, Quote>("UPDATE quotes SET author = $1, quote = $2, version = version + 1 WHERE id = $3 RETURNING *")
         .bind(req.author)
         .bind(req.quote)
         .bind(id)
@@ -83,7 +83,7 @@ pub async fn undo(
         .await
     {
         Ok(q) => Ok(Json(q)),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+        Err(_) => Err(StatusCode::NOT_FOUND),
     }
 }
 
@@ -97,18 +97,16 @@ pub struct QuoteReq {
 pub async fn draft(
     State(pool): State<Arc<PgPool>>,
     Json(req): Json<QuoteReq>,
-) -> Result<Json<Quote>, StatusCode>
+) -> Result<(StatusCode, Json<Quote>), StatusCode>
 {
-    match sqlx::query_as::<_, Quote>("INSERT TO quotes (id, author, quote, created_at, version) VALUES (?, ?, ?) RETURNING *")
+    match sqlx::query_as::<_, Quote>("INSERT INTO quotes (id, author, quote) VALUES ($1, $2, $3) RETURNING *")
         .bind(Uuid::new_v4())
         .bind(req.author)
         .bind(req.quote)
-        // .bind(Local::now())
-        // .bind(1)
         .fetch_one(&*pool)
         .await
     {
-        Ok(q) => Ok(Json(q)),
+        Ok(q) => Ok((StatusCode::CREATED, Json(q))),
         Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
